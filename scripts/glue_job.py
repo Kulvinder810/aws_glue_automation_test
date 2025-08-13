af258812678f0f73f0f5ca0a6aefa57e96526307
Kulvinder Singh <kuls810@gmail.com>
2025-08-13 18:04:55 +0530

    hello 1

--- scripts/glue_job.py
+++ scripts/glue_job.py
@@ -1,1 +1,7 @@
-print("Hello World from Glue!")
+import boto3
+
+s3 = boto3.client('s3')
+bucket_name = 'trendytech-tutorial-bucket'
+key = 'output/hello_world_output.txt'
+
+s3.put_object(Bucket=bucket_name, Key=key, Body='Hello World from Glue!\n')
