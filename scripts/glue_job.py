32692655b78918af11bc6143365a4247bbfc64c5
Kulvinder Singh <kuls810@gmail.com>
2025-08-13 18:03:11 +0530

    hello

--- scripts/glue_job.py
+++ scripts/glue_job.py
@@ -1,7 +1,1 @@
-import sys
-from awsglue.utils import getResolvedOptions
-
-args = getResolvedOptions(sys.argv, ['JOB_NAME'])
-
-print("Hello, AWS Glue!")
-print(f"This job's name is: {args['JOB_NAME']}")
+print("Hello World from Glue!")
